e7f3a02661719cf9e86b215ae577db0769151877
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-04-16 22:33:36 +0700

    refactor: rename Dashboard.py to dashboard.py

--- src/ui/manage_transaction.py
+++ src/ui/manage_transaction.py
@@ -1,6 +1,5 @@
 """Manage Transaction Page Module"""
 import datetime
-from src.ui.dashboard import WelcomeMessage
 
 from flet_core import (
     UserControl,
@@ -13,8 +12,17 @@ from flet_core import (
     ScrollMode,
     Row,
     Icon,
-    alignment,DataTable, DataRow,DataColumn,DataCell, TextAlign,FontWeight, Image
+    alignment,
+    DataTable,
+    DataRow,
+    DataColumn,
+    DataCell,
+    TextAlign,
+    FontWeight,
+    Image,
 )
+
+
 class History(UserControl):
     def __init__(
         self,
@@ -22,7 +30,7 @@ class History(UserControl):
         title: str = "payment",
         date: datetime = datetime.date.today(),
         nominal: float = 0.0,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(**kwargs)
         self.icon = icon
@@ -47,6 +55,8 @@ class History(UserControl):
                 ),
             ],
         )
+
+
 class RecentTransactions(UserControl):
     """Recent Transactions's Card"""
 
@@ -226,80 +236,80 @@ class RecentTransactions(UserControl):
             ),
         )
 
+
 class TransactionsDiagram(UserControl):
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def build(self):
-        return(
-            Container(
-                content=Row(
-                    controls=[
-                        Container(
-                            content=PieChart(
-                                center_space_radius=40,
-                                sections=[
-                                    PieChartSection(
-                                        value=31.8,
-                                        title="31.8%",
-                                        color="red",
-                                        radius=70,
-                                    ),
-                                    PieChartSection(
-                                        value=18.2,
-                                        title="18.2%",
-                                        color="yellow",
-                                        radius=70,
-                                    ),
-                                    PieChartSection(
-                                        value=22.7,
-                                        title="22.7%",
-                                        color="blue",
-                                        radius=70,
-                                    ),
-                                    PieChartSection(
-                                        value=27.3,
-                                        title="27.3%",
-                                        color="green",
-                                        radius=70,
-                                    ),
-                                ],
-                            ),
+        return Container(
+            content=Row(
+                controls=[
+                    Container(
+                        content=PieChart(
+                            center_space_radius=40,
+                            sections=[
+                                PieChartSection(
+                                    value=31.8,
+                                    title="31.8%",
+                                    color="red",
+                                    radius=70,
+                                ),
+                                PieChartSection(
+                                    value=18.2,
+                                    title="18.2%",
+                                    color="yellow",
+                                    radius=70,
+                                ),
+                                PieChartSection(
+                                    value=22.7,
+                                    title="22.7%",
+                                    color="blue",
+                                    radius=70,
+                                ),
+                                PieChartSection(
+                                    value=27.3,
+                                    title="27.3%",
+                                    color="green",
+                                    radius=70,
+                                ),
+                            ],
                         ),
-                        Container(
-                            expand=True,
-                            bgcolor="white",
-                            padding=Padding(20, 20, 20, 20),
-                            border_radius=20,
-                            content=Row(
-                                controls=[
-                                    Column(
-                                        expand=True,
-                                        controls=[
-                                            Text("History", size=30),
-                                            Column(
-                                                scroll=ScrollMode.AUTO,
-                                                expand=True,
-                                                spacing=5,
-                                                controls=[
-                                                    History(),
-                                                    History(),
-                                                    History(),
-                                                    History(),
-                                                    History(),
-                                                    History(),
-                                                    History(),
-                                                ],
-                                            ),
-                                        ],
-                                    ),
-                                ],
-                            ),
+                    ),
+                    Container(
+                        expand=True,
+                        bgcolor="white",
+                        padding=Padding(20, 20, 20, 20),
+                        border_radius=20,
+                        content=Row(
+                            controls=[
+                                Column(
+                                    expand=True,
+                                    controls=[
+                                        Text("History", size=30),
+                                        Column(
+                                            scroll=ScrollMode.AUTO,
+                                            expand=True,
+                                            spacing=5,
+                                            controls=[
+                                                History(),
+                                                History(),
+                                                History(),
+                                                History(),
+                                                History(),
+                                                History(),
+                                                History(),
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                            ],
                         ),
-                    ]
-                )
+                    ),
+                ]
             )
         )
+
 
 class ManageTransaction(UserControl):
     def __init__(self, **kwargs):
@@ -308,10 +318,5 @@ class ManageTransaction(UserControl):
     def build(self):
         return Column(
             spacing=0,
-            controls=[
-                RecentTransactions()
-            ],
+            controls=[RecentTransactions()],
         )
-
-
-
