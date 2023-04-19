c98f3baca92177aa6b85856a8bb939b907c9abfb
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-04-19 22:59:24 +0700

    feat: add artikel konten

--- src/ui/artikel_konten.py
+++ src/ui/artikel_konten.py
@@ -1,6 +1,7 @@
 import os
 os.system("")
-artikel = [{"judul" : "Artikel 1","konten" : """
+artikel = [
+    {"judul" : "Pengelolaan Aset","konten" : """
         Salah satu kemampuan yang harus dimiliki oleh orang-orang saat ini adalah kemampuan dalam mengelola aset pribadinya, terutama dalam mengelola aset keuangan pribadi atau dikenal dengan personal finance. Hal ini dilakukan agar di masa tua nanti, kondisi finansial telah terjamin karena sudah mengatur keuangan pribadinya sejak dini.
 
         Mulailah mengatur keuangan pribadi secepat mungkin, yaitu dengan mengetahui mana yang jadi kebutuhan dan mana yang hanya sekedar keinginan saja. Lalu, apa itu personal finance? Personal finance adalah proses perencanaan dan pengelolaan keuangan serta tabungan dan investasi pribadi seseorang.
@@ -84,5 +85,113 @@ artikel = [{"judul" : "Artikel 1","konten" : """
 
         Oiya, cara lainnya dari mengatur keuangan pribadi yang dapat kamu lakukan dan akan memberikan manfaat jangka panjang adalah dengan melakukan investasi. Dari investasi, nantinya kamu berpotensi mendapatkan keuntungan yang dapat kamu simpan untuk masa pensiun nanti
     """},
+    {"judul": "Baru Mulai Atur Keuangan? Coba Tips Ini", "konten":"""
+    
+    Melansir dari laman the balance, Senin (25/4/2021), panduan empat langkah ini akan membantu Anda menentukan kekayaan bersih, menetapkan tujuan keuangan, melacak pengeluaran, dan membuat anggaran.
+
+    1. Tentukan Kekayaan Bersih
+
+    Kekayaan bersih yakni total semua aset dikurangi kewajiban. Ini dapat memberi tahu banyak hal tentang kesehatan keuangan saat ini, dan itu juga dapat membantu untuk merencanakan masa depan keuangan Anda.
+
+    Kekayaan bersih adalah uang tunai yang akan dimiliki jika menjual semua yang ada dan melunasi semua utang. Saat membuat perhitungan kekayaan bersih, barang-barang yang dianggap sebagai aset yakni tabungan dan rekening koran, investasi (saham, obligasi, sekuritas, dan akun pensiun), kendaraan, perhiasan, dan barang berharga lainnya.
+
+    Item berikut adalah kewajiban yang harus diselesaikan yakni utang kartu kredit, saldo pinjaman pelajar, saldo yang tersisa pada hipotek atau pinjaman mobil, dan utang lainnya.
+
+    Setelah itu, tambahkan nilai aset, lalu jumlahkan nilai kewajiban. Kurangi kewajiban dari aset, dan jumlah yang didapatkan kembali adalah kekayaan bersih yang Anda miliki.
+
+    Setelah mengetahui berapa kekayaan bersih saat ini, biasakan untuk menghitung ulang kekayaan bersih setiap tahun atau setiap kali ada perubahan signifikan pada keuangan pribadi.
+
+    
+
+    2. Tetapkan Tujuan Keuangan
+
+    Untuk mengatur keuangan, pertama-tama putuskan apa yang ingin Anda capai. Luangkan waktu untuk mengidentifikasi tujuan keuangan dari yang besar sampai yang kecil sekalipun. Kemudian, pikirkan tentang bagaimana Anda akan mencapai tujuan tersebut.
+
+    Rencana keuangan dapat membantu bersiap menghadapi pengeluaran besar dengan sedikit kekhawatiran. Jika Anda meluangkan waktu untuk menanam benih untuk masa depan dengan membuat rencana dengan tujuan yang jelas dan garis waktu tertentu, Anda akan menuai hasilnya bertahun-tahun kemudian.
+
+    
+
+    3. Selalu Lacak Pengeluaran
+
+    Tahukah Anda berapa banyak uang yang dibelanjakan setiap bulan? Jika belum, sekarang saatnya mencari tahu.
+
+    Mengetahui ke mana perginya uang dapat membantu memberikan gambaran yang jelas tentang kebiasaan belanja Anda.
+
+    Apakah menghabiskan terlalu banyak uang untuk hal-hal yang tidak penting, atau Anda tertinggal dalam tujuan menabung atau membelanjakan lebih dari yang dihasilkan. Melihat angka-angka pasti mungkin menunjukkan bahwa hal-hal kecil benar-benar bertambah.
+
+    Pada akhir bulan, akan ada gambaran yang lebih jelas tentang kemana uang dibelanjakan dan mengetahui bagian mana yang mungkin dapat dipotong.
+
+    
+
+    4. Buat Anggaran
+
+    Setelah membuat daftar tujuan finansial dan mengamati kebiasaan pengeluaran, inilah saatnya membuat anggaran yang mencerminkan bagaimana Anda ingin membelanjakan uang.
+
+    Untuk membuat anggaran yang efektif, mulailah dengan lembar kerja anggaran, kumpulkan semua laporan keuangan, catat sumber pendapatan, buat daftar pengeluaran bulanan, dan membuat penyesuaian pada pengeluaran tersebut.
+
+    Dengan segala pencatatan yang baik dan rapi maka, arus keuangan baik pendapatan dan pengeluaran akan terlacak dengan jelas dan itu artinya akan memudahkan Anda.
+    
+    """},
+    {"judul": "7 Langkah Merdeka Finansial", "konten":"""
+    Untuk meningkatkan peluang sukses menuju merdeka finansial, Anda perlu mengikuti 7 pendekatan strategis, dilansir dari laman Citibank.ae, Jumat (14/5/2021), berikut rinciannya:
+
+    1. Tentukan apa arti sukses bagi Anda
+
+    Jika Anda melihat kembali kehidupan Anda pada tahap selanjutnya, apa yang ingin Anda capai? Mengapa itu penting bagi Anda? Setelah Anda menjawab pertanyaan-pertanyaan ini, ukurlah secara konkret. Lampaui gagasan kebebasan finansial yang samar-samar.
+
+    Tidaklah cukup hanya menginginkan sebuah rumah sendiri, mencoba dan mendefinisikan seperti apa rumah, di mana Anda menginginkannya, dan bagaimana memilikinya akan mengubah hidup Anda; Atau mungkin Anda ingin membangun dana darurat senilai biaya enam bulan.
+
+    Dengan memahami dan mengklasifikasi keinginan, Anda menetapkan tujuan yang sesuai dengan nilai-nilai pribadi Anda. Sukses membutuhkan komitmen dan ketekunan. Jika tujuan Anda tidak mencerminkan keyakinan intrinsik Anda, kecil kemungkinan Anda akan berusaha mewujudkannya.
+
+    
+
+    2. Periksalah situasi pribadi Anda
+
+    Setelah mengetahui tujuan, Anda dapat memetakan rute untuk sampai ke sana. Untuk melakukannya, ada baiknya jika Anda memiliki gambaran yang jelas tentang situasi keuangan Anda saat ini. Dengan menggunakan kertas atau spreadsheet, nilai pendapatan, pengeluaran, investasi, dan kekayaan bersih Anda.
+
+    Pemahaman yang jelas tentang elemen-elemen ini akan menunjukkan ukuran tugas di depan Anda dan memungkinkan Anda memprioritaskan tujuan Anda. Jika Anda tidak memiliki pekerjaan atau sumber pendapatan tetap, misalnya, hal itu menjadi prioritas di atas segalanya.
+
+    
+
+    3. Pertimbangkan tiga jenis tujuan
+
+    Pakar keuangan merekomendasikan untuk menetapkan target untuk jangka pendek, menengah, dan panjang. Dengan meletakkan kerangka waktu di sekitar tujuan Anda, Anda dapat mengatur kecepatan diri Anda sendiri dan hindari kekesalan karena tidak dapat mencapai sesuatu dengan cukup cepat.
+
+    Target jangka pendek adalah item yang lebih kecil, item yang mudah ditindaklanjuti yang meningkatkan kepercayaan diri Anda dan memacu Anda ke item dengan tiket lebih besar yang membutuhkan lebih banyak waktu.
+
+    Kemenangan cepat seperti itu harus menjadi hal yang dapat Anda capai dalam satu tahun dan mungkin termasuk membuat dana darurat, melunasi kartu kredit Anda atau membuka rencana investasi yang sistematis.
+
+    Tujuan jangka menengah membutuhkan tiga sampai lima tahun untuk membuahkan hasil. Contohnya termasuk melunasi pinjaman siswa Anda, memulai bisnis, mengumpulkan uang untuk uang muka pinjaman rumah atau merencanakan liburan impian.
+
+    Ambisi yang lebih besar, seperti membeli rumah, merupakan rencana jangka panjang karena ukuran tiketnya yang lebih besar. Niat seperti itu biasanya juga termasuk menyisihkan uang untuk masa pensiun Anda atau meninggalkan warisan untuk keluarga Anda atau dana abadi untuk tujuan favorit Anda.
+
+    
+
+    4. Tetapkan Rumus SMART
+
+    Saat menyusun rencana Anda, beri nilai berdasarkan parameter berikut: Specific (Spesifik), Measurable (Terukur), Achievable (tercapai), Relevant (Relevan), Timely (Tepat waktu).
+
+    Setiap tujuan harus sesuai dengan masing-masing parameter ini jika Anda ingin memiliki peluang sukses. Dengan kerangka kerja SMART, Anda mengetahui rupa kesuksesan, cara mencapainya, sumber daya apa yang Anda butuhkan, dan pencapaian apa yang menunjukkan kemajuan di sepanjang jalan.
+
+    
+
+    5. Tulislah
+
+    Setelah Anda mengidentifikasi tujuan Anda, tuliskan tujuan tersebut. Atau buat papan visi dan gantung di atas meja Anda. Langkah penting ini memungkinkan Anda untuk menjaga ambisi Anda tetap terorganisir dan nyata dan berfungsi sebagai stimulan visual untuk mencapai impian Anda.
+
+    
+
+    6. Alokasikan sumber daya untuk setiap tujuan
+
+    Jadi, Anda tahu apa tujuan Anda, dan Anda memiliki gambaran yang jelas tentang tujuan. Langkah terpenting adalah mengalokasikan sebagian dari pendapatan bulanan Anda untuk setiap tujuan. Mulailah dengan tujuan jangka pendek dan cari jalan keluarnya. Rencanakan untuk mengatur debit langsung untuk target langsung dan hentikan pengeluaran yang Anda anggap tidak perlu.
+
+    
+
+    7. Tinjau kemajuan Anda
+
+    Mungkin bagian terpenting dari latihan penetapan tujuan adalah evaluasi berkala tentang bagaimana keadaannya. Jadwalkan beberapa jam sekali setiap bulan dan lingkari tanggal di kalender Anda.
+
+    Periksa kinerja Anda, apakah Anda dapat memenuhi tujuan bulan ini, dan perilaku apa yang perlu Anda sesuaikan ke depannya. Tinjauan rutin membuat Anda tetap up to date dan pada jalurnya, memungkinkan Anda memperbarui dan mengubahnya dengan eksposur keuangan minimal.
+    """}
     
 ]
